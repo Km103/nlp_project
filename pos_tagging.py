4ffec6055d23b4e84da8916cbc8735af235c56cc
Km103 <21ucs103@lnmiit.ac.in>
2023-10-24 16:48:58 +0530

    nltk.download changes

--- pos_tagging.py
+++ pos_tagging.py
@@ -1,12 +1,12 @@
 import nltk
-# nltk.download('punkt')
-# nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-# #download the treebank corpus from nltk
-# nltk.download('treebank')
+#download the treebank corpus from nltk
+nltk.download('treebank')
 
-# #download the universal tagset from nltk
-# nltk.download('universal_tagset')
+#download the universal tagset from nltk
+nltk.download('universal_tagset')
 
 def tagging(tokens):
     #performs POS tagging using Penn Treebank
